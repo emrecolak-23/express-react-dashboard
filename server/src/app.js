d4392003027aa39312b89ce8264b045f7361a2eb
Emre ÇOLAK <colakkemre@gmail.com>
2023-04-27 22:50:20 +0300

    creating launches

--- server/src/app.js
+++ server/src/app.js
@@ -23,8 +23,8 @@ app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routes Middlewares
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
