a19b804bc24676c726b8b5818bc67715966a194a
Emre ÇOLAK <colakkemre@gmail.com>
2023-04-27 20:33:14 +0300

    serving react.js front end in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
-const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'http://localhost:8000'];
 
 // Middlewares
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (whitelist.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
@@ -23,5 +25,9 @@ app.use(
 
 // Routes Middlewares
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
