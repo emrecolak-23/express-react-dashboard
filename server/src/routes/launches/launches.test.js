0bd3a2fdf3fe450f82b1ab8c75a03e5914bee47c
Emre ÇOLAK <colakkemre@gmail.com>
2023-04-28 22:36:06 +0300

    testing api endpoints with jest and supertest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,62 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+  it('Should responde with 200 success', async () => {
+    await request(app)
+      .get('/launches')
+      .send()
+      .expect('Content-Type', /json/)
+      .expect(200);
+    // expect(response.status).toBe(200);
+  });
+});
+
+describe('Test POST /launches', () => {
+  const completeLaunch = {
+    mission: 'ZTM155',
+    rocket: 'ZTM Experimental IS1',
+    launchDate: 'January 17, 2030',
+    target: 'Kepler-186 f',
+  };
+
+  it('Should respond with 200 success', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunch)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunch.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(requestDate).toBe(responseDate);
+
+    delete completeLaunch.launchDate;
+    expect(response.body).toMatchObject(completeLaunch);
+  });
+
+  it('Should catch missing required property', async () => {
+    delete completeLaunch.launchDate;
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunch)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'Missing required launch property',
+    });
+  });
+
+  it('Should catch invalid dates', async () => {
+    completeLaunch.launchDate = 'zoot';
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunch)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'Invalid launch date',
+    });
+  });
+});
