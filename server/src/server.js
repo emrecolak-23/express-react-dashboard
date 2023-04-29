71d8f80cebebbb96adc6f800c6a906e95f28ad0b
Emre ÇOLAK <colakkemre@gmail.com>
2023-04-30 00:47:33 +0300

    connect mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,24 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
+const MONGO_URI =
+  'mongodb+srv://emrecolak:emco3232@cluster0.awtf9.mongodb.net/Nasa-Project?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(error);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URI);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
