59314281fbdf7328f439e1d209c74cfbcf1ce9be
Emre ÇOLAK <colakkemre@gmail.com>
2023-04-30 12:37:44 +0300

    creating mongoose schema for launches

---
+++ server/src/models/launches.schema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const launchesSchema = new Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: {
+    type: [String],
+  },
+  upcoming: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
